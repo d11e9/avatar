d38988a7f9988d1b7f546b6adedf721f7e8413ae
Doug A. <d11e9@users.noreply.github.com>
2015-08-27 17:11:45 +0100

    Remove wot and sender, not in AIP-0 or AIP-1

--- contracts/avatar.sol
+++ contracts/avatar.sol
@@ -25,55 +25,6 @@ contract mortal is owned {
     function kill() onlyowner { suicide(owner); }
 }
 
-contract wot is owned, mortal {
-    struct Rating {
-        int value;
-        address prev;
-        address next;
-    }
-    
-    struct WoT {
-        address head;
-        address tail;
-        mapping (address => Rating) ratings;
-    }
-    
-    WoT public wot;
-
-    function getRating( address id) constant returns (int rating) {
-        return wot.ratings[id].value;
-    }
-    
-    function wotUp(address id) onlyowner returns (int) {
-        if (wot.ratings[id].value != 0) {
-            wot.ratings[id].value++;
-        } else {
-            wot.ratings[id] = Rating( 1, wot.head, address(0));
-            wot.head = id;
-        }
-        return wot.ratings[id].value;
-    }
-    function wotDown (address id) onlyowner returns (int) {
-        if (wot.ratings[id].value != 0) {
-            wot.ratings[id].value--;
-        } else {
-            wot.ratings[id] = Rating( -1, wot.head, address(0));
-            wot.head = id;
-        }
-        return wot.ratings[id].value;
-    }
-}
-
-contract sender is owned {
-    function send(address to, uint value) onlyowner returns (bool success) {
-        if (address(this).balance >= value) {
-            to.send( value );
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
 
 contract keyvalue is owned {
     mapping (bytes32 => string) public keys;
@@ -82,7 +33,7 @@ contract keyvalue is owned {
     }
 }
 
-contract Avatar is wot, sender, keyvalue { }
+contract Avatar is keyvalue { }
 
 contract AvatarFactory {
     
