3ec83672bc6bd960b9d02af736dc46ee2759fb80
d11e9 <d11e9@turkd.net>
2015-09-07 10:29:09 +0100

    remove dist and consolidate contracts

--- contracts/avatar.sol
+++ contracts/avatar.sol
@@ -1,20 +1,9 @@
 contract owned {
     address owner;
     function owned() { owner = msg.sender; }
-    function transfer(address to) returns (bool success){
-        if (isOwner()) {
-            owner = to;
-            return true;
-        } else {
-            return false;
-        }
-    }
-    function isOwner () constant returns(bool) {
-        return msg.sender == owner;
-    }
-    
-    function isOwner (address addr) constant returns(bool) {
-        return addr == owner;
+    function transfer(address to) onlyowner returns (bool success){
+        owner = to;
+        return true;
     }
     
     modifier onlyowner { if (msg.sender == owner) _ }
@@ -28,19 +17,104 @@ contract mortal is owned {
 }
 
 contract keyvalue is owned, mortal {
-    mapping (bytes32 => string) keys;
+    mapping (bytes32 => bytes32) keys;
     
-    function get(bytes32 key) constant returns (string) {
+    function get(bytes32 key) constant returns (bytes32) {
         return keys[key];
     }
-    function set(bytes32 key, string value) onlyowner returns(string) {
+    
+    function set(bytes32 key, bytes32 value) onlyowner returns(bytes32) {
         keys[key] = value;
         return keys[key];
     }
 }
+contract LinkedListAddressInt is owned, mortal {
+    
+    struct Item {
+        bool exists;
+        address prev;
+        address next;
+        int value;
+    }
+    
+    address public head;
+    address public tail;
+    uint public length;
+    mapping( address => Item) public items;
+    
+    function next(address id) constant returns(address) { return items[id].next; }
+    function prev(address id) constant returns(address) { return items[id].prev; }
+    
+    modifier exist (address id, bool flag) { if ((items[id].value != 0 ) == flag) _ }
+
+    function add (address addr, int value) exist(addr, false) internal returns(int) {
+        address prev = head;
+        items[prev].next = addr;
+        items[addr] = Item( true, prev, address(0), value );
+        head = addr;
+        length++;
+        
+        if (tail == address(0)) { tail = addr; }
+        return value;
+    }
+    
+    function remove(address addr) exist(addr, true) internal {
+        if (head == addr) {
+            head = items[addr].prev;
+        }
+        
+        if (tail == addr) {
+            tail = items[addr].next;
+        }
+        
+        if (items[addr].prev != address(0)) {
+            items[items[addr].prev].next = items[addr].next;
+        }
+        
+        if (items[addr].next != address(0)) {
+            items[items[addr].next].prev = items[addr].prev;
+        }
+        
+        length--;
+        
+        delete items[addr];
+        
+    }
+}
+
+
+contract WoT is LinkedListAddressInt {
+
+    function up (address id) onlyowner returns (int rating) {
+      if (items[id].exists) {
+          items[id].value++;
+      } else {
+          add( id, 1 );
+      }
+      return items[id].value;
+    }
+    function down (address id) onlyowner returns (int rating) {
+      if (items[id].exists) {
+          items[id].value--;
+      } else {
+          add( id, -1 );
+      }
+      return items[id].value;
+    }
+    function set (address id, int value) onlyowner returns (int rating) {
+      if (items[id].exists) {
+          items[id].value = value;
+      } else {
+          add( id, value );
+      }
+      return items[id].value;
+    }
+
+}
+
 
 contract Avatar is keyvalue {
-
+    
 }
 
 
@@ -53,32 +127,24 @@ contract LinkedListAddressAddress is owned, mortal {
         address value;
     }
     
-    address head;
-    address  tail;
-    uint length;
-    mapping( address => Item) items;
+    address public head;
+    address public tail;
+    uint public length;
+    mapping( address => Item) public items;
     
-    function getHead () constant returns (address) { return head; }
-    function getTail () constant returns (address) { return tail; }
-    function getLength () constant returns (uint) { return length; }
-    function getItem(address id) constant returns(address[] item) {
-        item[0] = items[id].prev;
-        item[1] = items[id].next;
-        item[2] = items[id].value;
-        return item;
-    }
+    function next(address id) constant returns(address) { return items[id].next; }
+    function prev(address id) constant returns(address) { return items[id].prev; }
     
     modifier exist (address id, bool flag) { if (items[id].exists == flag) _ }
     
     function add (address addr, address value) exist(addr, false) internal returns(address) {
-        address prev = head;
-        items[prev].next = addr;
-        items[addr] = Item( true, prev, address(0), value );
+        items[head].next = addr;
+        items[addr] = Item( true, head, address(0), value );
         head = addr;
         length++;
         
         if (tail == address(0)) { tail = addr; }
-        return addr;
+        return value;
     }
     
     function remove(address addr) exist(addr, true) internal returns(bool) {
@@ -108,10 +174,16 @@ contract LinkedListAddressAddress is owned, mortal {
 
 contract AvatarAggregator is LinkedListAddressAddress {
     
-    function create () returns(address contractAddress) {
+    function createAvatar () returns(address contractAddress) {
         Avatar avatar = new Avatar();
         avatar.transfer( msg.sender );
         return add( msg.sender, address(avatar) );
+    }
+    
+    function createWoT() onlyowner returns(address wotContractAddress) {
+        WoT wot = new WoT();
+        wot.transfer( msg.sender );
+        return address(wot);
     }
     
     function register (address avatar) returns (address contractAddress) {
